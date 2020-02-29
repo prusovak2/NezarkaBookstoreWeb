bf3e84fd0eb4bf22e0f859744ebd5448c5a66b62
prusovak2 <prusova.k2@seznam.cz>
2020-02-29 19:27:55 +0100

    Initial commit, JumpingPlatformGame solution added.

---
+++ JumpingPlatformGame/JumpingPlatformGame/Entities.cs
@@ -0,0 +1,114 @@
+﻿using System.Drawing;
+
+namespace JumpingPlatformGame {
+	class Entity {
+        public WorldPoint Location;
+		public virtual Color Color => Color.Black;
+		
+		public void Update(Seconds deltaSeconds)
+		{
+			if(this is MovableEntity movableEntity)
+			{
+				Meters horizontalDelta = deltaSeconds * movableEntity.Horizontal.Speed;
+				Meters newXlocation = movableEntity.Location.X + horizontalDelta;
+
+				//if you are leaving the screen 
+				if (newXlocation >= movableEntity.Horizontal.UpperBound)					
+				{
+					//change direction
+					movableEntity.Horizontal.Speed *=(-1);
+					//move as far as you can
+					movableEntity.Location.X = movableEntity.Horizontal.UpperBound;
+				}
+				else if(newXlocation <= movableEntity.Horizontal.LowerBound)
+				{
+					//change direction
+					movableEntity.Horizontal.Speed *= (-1);
+					//move as far as you can
+					movableEntity.Location.X = movableEntity.Horizontal.LowerBound;
+				}
+				else
+				{
+					//just move
+					movableEntity.Location.X += horizontalDelta;
+				}
+				
+				
+			}
+			if (this is MovableJumpingEntity movableJumpingEntity)
+			{
+				Meters verticalDelta = deltaSeconds * movableJumpingEntity.Vertical.Speed;
+				Meters newYlocation = movableJumpingEntity.Location.Y + verticalDelta;
+				//if you are tryint to fly away from the screen
+				if (newYlocation >= movableJumpingEntity.Vertical.UpperBound)
+				{
+					//change direction
+					movableJumpingEntity.Vertical.Speed *= (-1);
+					//move as far as you can
+					movableJumpingEntity.Location.Y = movableJumpingEntity.Vertical.UpperBound;
+				}
+				//if you reach the ground
+				else if (newYlocation <= movableJumpingEntity.Vertical.LowerBound)
+				{
+					//just stop falling
+					movableJumpingEntity.Location.Y = movableJumpingEntity.Vertical.LowerBound;
+				}
+				else
+				{
+					//just move
+					movableJumpingEntity.Location.Y += verticalDelta;
+				}
+
+			}
+		}
+	}
+
+	class MovableEntity : Entity {
+        public Movement Horizontal;
+		public MovableEntity()
+		{
+			this.Horizontal = new Movement();
+		}
+	}
+
+	class MovableJumpingEntity : MovableEntity {
+        public Movement Vertical;
+		public MovableJumpingEntity():base()
+		{
+			this.Vertical = new Movement();
+		}
+	}
+
+	class Joe : MovableEntity {
+		public override string ToString() => "Joe";
+		public override Color Color => Color.Blue;
+	}
+
+	class Jack : MovableEntity {
+		public override string ToString() => "Jack";
+		public override Color Color => Color.LightBlue;
+	}
+
+	class Jane : MovableJumpingEntity {
+		public override string ToString() => "Jane";
+		public override Color Color => Color.Red;
+	}
+
+	class Jill : MovableJumpingEntity {
+		public override string ToString() => "Jill";
+		public override Color Color => Color.Pink;
+	}
+
+    public class WorldPoint
+    {
+        public Meters X;
+        public Meters Y;
+    }
+
+    public class Movement
+    {
+        public Meters LowerBound;
+        public Meters UpperBound;
+        public MeterPerSeconds Speed;
+    }
+}
---
+++ JumpingPlatformGame/JumpingPlatformGame/Physics.cs
@@ -0,0 +1,155 @@
+﻿namespace JumpingPlatformGame 
+{
+    public static class ExtensionsInt
+    {
+        public static Meters Meters(this int what)
+        {
+            return new Meters(what);
+        }
+        public static Seconds Seconds(this int what)
+        {
+            return new Seconds(what);
+        }
+        public static MeterPerSeconds MeterPerSeconds(this int what)
+        {
+            return new MeterPerSeconds(what);
+        }
+    }
+
+    public static class ExtensionDouble
+    {
+        public static MeterPerSeconds MeterPerSeconds(this double what)
+        {
+            return new MeterPerSeconds(what);
+        }
+        public static Seconds Seconds(this double what)
+        {
+            return new Seconds(what);
+        }
+        
+    }
+
+
+    public struct Meters
+    {
+        public double Value;
+        public Meters(double i)
+        {
+            this.Value = i;
+        }
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+        public static MeterPerSeconds operator /(Meters meters, Seconds seconds)
+        {
+            return new MeterPerSeconds(meters.Value / seconds.Value);
+        }
+        public static bool operator <=(Meters m1, Meters m2)
+        {
+            if (m1.Value <= m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >(Meters m1, Meters m2)
+        {
+            if (m1.Value > m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator <(Meters m1, Meters m2)
+        {
+            if (m1.Value < m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator >=(Meters m1, Meters m2)
+        {
+            if (m1.Value >= m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator ==(Meters m1, Meters m2)
+        {
+            if (m1.Value == m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Meters m1, Meters m2)
+        {
+            if (m1.Value != m2.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+        public override bool Equals(object o)
+        {
+            if (o == null)
+            {
+                return false;
+            }
+            if(o is Meters m)
+            {
+                if (this.Value == m.Value)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        public static Meters operator *(Meters m, int i)
+        {
+            return new Meters(m.Value * i);
+        }
+        public static Meters operator +(Meters m1, Meters m2)
+        {
+            return new Meters(m1.Value + m2.Value);
+        }
+
+
+    }
+    public struct Seconds
+    {
+        public double Value;
+        public Seconds(double i)
+        {
+            this.Value = i;
+        }
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+    public struct MeterPerSeconds //MetersPerSecond?
+    {
+        public double Value;
+        public MeterPerSeconds(double i)
+        {
+            this.Value = i;
+        }
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+        public static Meters operator *(Seconds seconds, MeterPerSeconds mps)
+        {
+            return new Meters(seconds.Value * mps.Value);
+        }
+        public static MeterPerSeconds operator *(MeterPerSeconds mps, int i)
+        {
+            return new MeterPerSeconds(mps.Value * i);
+        }
+    }
+}
